744b87bf3b5f1ad583d91f1461bae0c558ae76b2
Allyson Araujo <allyson.m.araujo@gmail.com>
2024-04-20 21:03:31 -0300

    commit inicial

---
+++ src/ContaBancaria.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ContaBancaria {
+    public static void ContaBancaria(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        String nome = "Fulano Ciclano da Silva";
+        String tipoConta = "Conta Corrente";
+        int saldoDaConta = 2500;
+        int opcaoEscolhida = 0;
+
+        while (opcaoEscolhida != 4){
+
+            System.out.println("***********************");
+            System.out.println("Dados iniciais do cliente:");
+            System.out.println(" ");
+            System.out.println("Nome: " + nome);
+            System.out.println("Tipo conta: " + tipoConta);
+            System.out.println("Saldo inicial: R$ " + saldoDaConta);
+            System.out.println("***********************");
+            System.out.println(" ");
+            System.out.println("Operações");
+            System.out.println(" ");
+            System.out.println("1- Consultar saldos");
+            System.out.println("2- Receber valor");
+            System.out.println("3- Transferir valor");
+            System.out.println("4- Sair");
+            System.out.println(" ");
+            System.out.println("Digite a opção desejada:");
+
+            opcaoEscolhida = leitura.nextInt();
+
+            switch (opcaoEscolhida){
+                case 1:
+                    System.out.println("Saldo Atual: R$ " + saldoDaConta);
+                case 2:
+                    System.out.println("Digite o valor a ser depositado: ");
+
+            }
+        }
+    }
+}
